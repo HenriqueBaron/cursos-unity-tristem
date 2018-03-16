312475140532507bf98145f4d77e616ebcc67577
Henrique Baron <henrique.baron@gmail.com>
2018-03-16 16:49:30 -0300

    FInalização do projeto Number Wizard com UI, e configuração inicial do projeto Block Breaker.

---
+++ NumberWizardUI/Assets/NumberWizard.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    int maximum;
+    int minimum;
+    int guess;
+
+    public Text GuessText;
+
+    const int maximumTries = 5;
+    int triesRemaining = maximumTries;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        maximum = 1000;
+        minimum = 1;
+        
+        NextGuess(true);
+
+    }
+
+    public void GuessHigher()
+    {
+        minimum = guess;
+        NextGuess();
+    }
+
+    public void GuessLower()
+    {
+        maximum = guess;
+        NextGuess();
+    }
+
+    void NextGuess(bool firstCall = false)
+    {
+        //guess = (maximum + minimum) / 2; Algoritmo antigo, utilizada o método da bisseção
+        guess = Random.Range(minimum, ++maximum);
+        GuessText.text = "Is your number higher, lower or equal to " + guess.ToString() + '?';
+        if (!firstCall)
+        {
+            triesRemaining--;
+            if (triesRemaining <= 0)
+            {
+                SceneManager.LoadScene("Lose");
+            }
+        }
+    }
+}

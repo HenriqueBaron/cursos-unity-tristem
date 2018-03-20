6ec455566218d2787e431e2d4ed81cdb202c4359
Henrique Baron <henrique.baron@gmail.com>
2018-03-20 12:42:44 -0300

    Desenvolvidos os componentes de física da primeira fase do Block Breaker e criados os prefabs dos tijolos.

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Paddle paddle;
+    private Vector3 paddleToBallVector;
+    private static bool hasStarted;
+
+    // Use this for initialization
+    void Start()
+    {
+        hasStarted = false;
+        paddleToBallVector = transform.position - paddle.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!hasStarted)
+        {
+            transform.position = paddle.transform.position + paddleToBallVector;
+            if (Input.GetMouseButtonDown(0))
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+                hasStarted = true;
+            }
+        }
+    }
+}
---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePositionInBlocks = Mathf.Clamp(Input.mousePosition.x / Screen.width * 16, 0.5f, 15.5f);
+        Vector3 newPaddlePos = new Vector3(mousePositionInBlocks, transform.position.y, transform.position.z);
+        transform.position = newPaddlePos;
+    }
+}

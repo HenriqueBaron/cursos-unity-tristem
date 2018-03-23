e4f615e98f4c5dccbc895c737a10b75a2f08a6c8
Henrique Baron <henrique.baron@gmail.com>
2018-03-23 16:43:24 -0300

    Criado o funcionamento dos tijolos a destruir: incluindo sprite sheet, a lógica que os destrói e o fim da fase ao destruir todos os tijolos.

--- Block Breaker/Assets/Scripts/Brick.cs
+++ Block Breaker/Assets/Scripts/Brick.cs
@@ -5,33 +5,49 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
-    public int maxHits;
-    private int timesHit;
+    public static int breakableCount = 0;
+    public Sprite[] hitSprites;
 
+    private bool isBreakable;
+    private int timesHit;
     private LevelManager levelManager;
 
     // Use this for initialization
     void Start()
     {
         levelManager = FindObjectOfType<LevelManager>();
+        if (tag == "Breakable")
+        {
+            isBreakable = true;
+            breakableCount++;
+        }
         timesHit = 0;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        timesHit++;
-        if (timesHit >= maxHits) Destroy(gameObject);
+        if (isBreakable) HandleHits();
     }
 
-    //TODO: Remove this method as soon as we can actually win.
-    private void SimulateWin()
+    private void HandleHits()
     {
-        levelManager.LoadNextLevel();
+        int maxHits = hitSprites.Length + 1;
+        timesHit++;
+        if (timesHit >= maxHits)
+        {
+            Destroy(gameObject);
+            breakableCount--;
+            levelManager.BrickDestroyed();
+        }
+
+        else LoadSprites();
+    }
+
+    private void LoadSprites()
+    {
+        if (hitSprites[timesHit - 1] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[timesHit - 1];
+        }
     }
 }

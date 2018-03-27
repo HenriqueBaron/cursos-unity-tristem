24371ff7faa7c6479963a64ef3d0a3028897aece
Henrique Baron <henrique.baron@gmail.com>
2018-03-27 11:49:26 -0300

    Criado o projeto Laser Defender.

---
+++ Laser Defender/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public static void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
---
+++ Laser Defender/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    static MusicPlayer player;
+
+    /* Observação: a ordem de execução dos métodos é importante. Awake é chamado antes de Start. */
+
+    void Awake()
+    {
+        if (player != null)
+        {
+            Destroy(gameObject);
+            print("Duplicate music player self-destructing!");
+        }
+        else
+        {
+            player = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+}

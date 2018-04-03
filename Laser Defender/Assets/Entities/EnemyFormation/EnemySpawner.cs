2826639feb10f6edce39a68f0476d45a3eb17610
Henrique Baron <henrique.baron@gmail.com>
2018-04-03 12:15:24 -0300

    Construída uma parte do Laser Defender.

---
+++ Laser Defender/Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float width = 10f;
+    public float height = 5f;
+    public Vector2 startVelocity = new Vector2(3, 0);
+
+    private float xMin;
+    private float xMax;
+    private Vector3 velocity;
+
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+            enemy.transform.parent = child;
+        }
+
+        velocity = (Vector3)startVelocity;
+
+        float zDistance = transform.position.z - Camera.main.transform.position.z;
+        xMin = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, zDistance)).x + width / 2;
+        xMax = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, zDistance)).x - width / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position + velocity * Time.deltaTime;
+
+        if (newPosition.x > xMax || newPosition.x < xMin)
+        {
+            velocity.x *= -1;
+            if (newPosition.x > xMax) newPosition.x = xMax;
+            else newPosition.x = xMin;
+        }
+
+        transform.position = newPosition;
+
+    }
+}
---
+++ Laser Defender/Assets/Entities/Player/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float shipSpeed = 5f;
+    public float limitPadding = 0.5f;
+    public GameObject laserPrefab;
+    public float projectileSpeed = 3;
+    public float fireRate = 0.2f;
+
+    private float xMin;
+    private float xMax;
+
+    void Start()
+    {
+        float zDistance = transform.position.z - Camera.main.transform.position.z;
+        xMin = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, zDistance)).x + limitPadding;
+        xMax = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, zDistance)).x - limitPadding;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePosition();
+        if (Input.GetKeyDown(KeyCode.Space)) InvokeRepeating("FireLaser", 0f, fireRate);
+        if (Input.GetKeyUp(KeyCode.Space)) CancelInvoke("FireLaser");
+    }
+
+    private void UpdatePosition()
+    {
+        Vector3 newPosition = transform.position;
+        if (Input.GetKey(KeyCode.RightArrow))
+            newPosition += Vector3.right * shipSpeed * Time.deltaTime;
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            newPosition += Vector3.left * shipSpeed * Time.deltaTime;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, xMin, xMax);
+        transform.position = newPosition;
+    }
+
+    private void FireLaser()
+    {
+        GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector3(0, projectileSpeed, 0);
+    }
+}
